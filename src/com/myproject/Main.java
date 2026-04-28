9ed03263c332543a7ba7a4e6f9b31b63605e7322
Edson Ney de Farias <edson.ney@gmail.com>
2026-04-28 15:41:35 -0300

    BigDecimal class

--- src/com/myproject/Main.java
+++ src/com/myproject/Main.java
@@ -1,42 +1,75 @@
 package com.myproject;
 
-import java.time.Duration;
-import java.time.OffsetDateTime;
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 public class Main {
     public static void main(String[] args) {
 
-        var stringStart = OffsetDateTime.now();
+        var value1 = 0.1;
+        var value2 = 0.2;
 
-        String stringConcat = "";
-        for (int i = 0; i < 400_000; i++) {
-            stringConcat += i;
-        }
+        System.out.println(value1 + value2);
 
-        var stringEnd = OffsetDateTime.now();
-        System.out.printf("String: %s \n", getInterval(stringStart, stringEnd));
+        System.out.println("=====================================");
 
-        var builderStart = OffsetDateTime.now();
-        var builderConcat = new StringBuilder();
-        for (int i = 0; i < 400_000; i++) {
-            builderConcat.append(i);
-        }
+        System.out.println(2.00 - 1.1);
+        System.out.println(2.00 - 1.2);
+        System.out.println(2.00 - 1.3);
+        System.out.println(2.00 - 1.4);
+        System.out.println(2.00 - 1.5);
+        System.out.println(2.00 - 1.6);
+        System.out.println(2.00 - 1.7);
+        System.out.println(2.00 - 1.8);
+        System.out.println(2.00 - 1.9);
 
-        var builderEnd = OffsetDateTime.now();
-        System.out.printf("StringBuilder (singlethread): %s \n", getInterval(builderStart, builderEnd));
+        System.out.println("=====================================");
 
-        var bufferStart = OffsetDateTime.now();
-        var bufferConcat = new StringBuffer();
-        for (int i = 0; i < 400_000; i++) {
-            bufferConcat.append(i);
-        }
+        var value3 = new BigDecimal(String.valueOf(value1));
+        var value4 = new BigDecimal(String.valueOf(value2));
 
-        var bufferEnd = OffsetDateTime.now();
-        System.out.printf("StringBuffer (multithread): %s \n", getInterval(bufferStart, bufferEnd));
-    }
+        System.out.println(value3.add(value4));
 
-    private static long getInterval(final OffsetDateTime springStart, final OffsetDateTime springEnd) {
-        return Duration.between(springStart, springEnd).toMillis();
+        System.out.println("=====================================");
+
+        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.1")));
+        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.2")));
+        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.3")));
+        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.4")));
+        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.5")));
+        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.6")));
+        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.7")));
+        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.8")));
+        System.out.println(new BigDecimal("2.00").subtract(new BigDecimal("1.9")));
+
+        System.out.println("=====================================");
+
+        var value5 = new BigDecimal("56.3645");
+        var value6 = new BigDecimal("99.345");
+        System.out.println(value5.multiply(value6));
+
+        System.out.println("=====================================");
+
+        var value7 = new BigDecimal("56.3678");
+        var value8 = new BigDecimal("99.34");
+        System.out.println(value8.divide(value7, 3, RoundingMode.HALF_UP));
+
+        System.out.println("=====================================");
+
+        System.out.println(value8.negate());
+        System.out.println(value8.abs());
+        System.out.println(value8.intValue());
+        System.out.println(value8.doubleValue());
+
+        System.out.println("=====================================");
+
+        var value9 = new BigDecimal("144");
+        System.out.println(value9.sqrt(new MathContext(2)));
+        var value10 = new BigDecimal("4");
+        System.out.println(value10.pow(2));
+
+        System.out.println("=====================================");
     }
 
 }
