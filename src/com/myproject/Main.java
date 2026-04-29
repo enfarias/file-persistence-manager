35447f2c328fcb981ea0e812c642d8b2ad05336d
Edson Ney de Farias <edson.ney@gmail.com>
2026-04-29 19:45:36 -0300

    Introduction to the Streams API Part 2

--- src/com/myproject/Main.java
+++ src/com/myproject/Main.java
@@ -1,30 +1,63 @@
 package com.myproject;
 
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
 
-        var value1 = Stream.generate(() -> new Random().nextInt())
-                .limit(5)
-                .toArray(Integer[]::new);
+        var value1 = Stream.of("Maria", "João", "Pedro", "Ana", "Lucas", "Luana", "Marcia", "Leandro")
+                .filter(name -> name.endsWith("o"))
+                .limit(2)
+                .toList();
+        System.out.println(value1);
 
-        for (var v : value1) {
-            System.out.println(v);
-        }
+        System.out.println("\n------------------------------------------\n");
 
-        System.out.println("\n=================================\n");
+        var value2 = Stream.of("Maria", "João", "Pedro", "Ana", "Lucas", "Luana", "Marcia")
+                .filter(n -> n.endsWith("o"))
+                .anyMatch(n -> n.contains("J"));
+        System.out.println(value2);
 
-        var value2 = IntStream.generate(() -> new Random().nextInt())
-                .limit(5)
-                .toArray();
+        System.out.println("\n------------------------------------------\n");
 
-        for (var v : value2) {
-            System.out.println(v);
-        }
+        var value3 = Stream.of("Maria", "João", "Pedro", "Ana", "Lucas", "Luana", "Marcia", "Leandro")
+                .reduce("", (a, b) -> a + b + ";");
+        System.out.println(value3);
 
+        System.out.println("\n------------------------------------------\n");
+
+        var value4 = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 7, 8, 7)
+                .distinct().toList();
+
+        System.out.println(value4);
+
+        System.out.println("\n------------------------------------------\n");
+
+        var value5 = Stream.of(1, 2, 3, 4, 5, 6, 7, 8)
+                .map(n -> n % 2 == 0)
+                .toList();
+
+        System.out.println(value5);
+
+        System.out.println("\n------------------------------------------\n");
+
+        List<Integer> values6 = List.of(3, 6, 9, 12);
+        List<Integer> values7 = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+
+        var newValues = values7.stream()
+                .filter(values6::contains)
+                .peek(n -> System.out.printf("Filter %s \n", n))
+                .map(n -> values6.stream().reduce(n, (n1, n2) -> n1 - n2))
+                .peek(n -> System.out.printf("Map %s \n", n))
+                .collect(Collectors.toSet());
+
+        System.out.println(newValues);
+
+        System.out.println("\n------------------------------------------\n");
     }
 
 }
