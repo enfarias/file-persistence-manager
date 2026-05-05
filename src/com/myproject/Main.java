35bc4488c8e4e7324a32478b42f89f2d7f39f6f2
Edson Ney de Farias <edson.ney@gmail.com>
2026-05-05 10:11:20 -0300

    Thread Class and Runnable Interface Part 1

--- src/com/myproject/Main.java
+++ src/com/myproject/Main.java
@@ -1,79 +1,42 @@
 package com.myproject;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.OffsetDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static List<Integer> numbers = new ArrayList<>();
 
-        LocalDate localDate = LocalDate.now();
-        LocalTime localtime = LocalTime.now();
-        LocalDateTime localDateTime = localDate.atTime(localtime);
-        OffsetDateTime offsetDateTime = OffsetDateTime.now();
-
-        System.out.println("1- " + offsetDateTime);
-
-        OffsetDateTime offsetDateTimeUTC = offsetDateTime.withOffsetSameInstant(ZoneOffset.UTC);
-
-        System.out.println("2- " + offsetDateTimeUTC);
-
-        offsetDateTime = offsetDateTime.withOffsetSameInstant(ZoneOffset.UTC);
-
-        System.out.println("3- " + offsetDateTime);
-        System.out.println("4- " + offsetDateTime.getOffset());
-        System.out.println("5- " + offsetDateTime.isEqual(offsetDateTimeUTC));
-
-        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
-        System.out.println("6- " + formatter.format(localDateTime));
-
-        Date date = new Date();
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        ZoneId zoneId = calendar.getTimeZone().toZoneId();
-        LocalDateTime localDateTime2 = localDateTime.ofInstant(calendar.toInstant(), zoneId);
-        System.out.println("7- " + localDateTime2);
-
-        System.out.println("8- " + Duration.between(localDateTime, localDateTime.now()).toMillis());
-
-        System.out.println("9- " + localDateTime.toInstant(ZoneOffset.ofHours(-3)));
-
-        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("hh:mm:ss");
-        System.out.println("10- " + timeFormatter.format(localtime));
-
-        System.out.println("11- " + localDate);
-        System.out.println("12- " + localtime);
-        System.out.println("13- " + localDate.plus(50, ChronoUnit.DAYS));
-        System.out.println("14- " + localDate.plusYears(20));
-        System.out.println("15- " + localDate.minusYears(20));
-        System.out.println("16- " + localDate.getMonth());
-        System.out.println("17- " + localDate.getMonthValue());
-
-        var strDate = "22/12/2015";
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        System.out.println("18- " + LocalDate.parse(strDate, dateFormatter));
-        System.out.println("19- " + dateFormatter.format(LocalDate.parse(strDate, dateFormatter)));
-
-        var date2 = Date.from(offsetDateTime.toInstant());
-        Calendar calendar2 = Calendar.getInstance();
-        calendar2.setTime(date2);
-        System.out.println("20- " + calendar2.getTime());
-
-        date = calendar.getTime();
-        OffsetDateTime offsetDateTime2 = date.toInstant().atOffset(ZoneOffset.ofHours(-3));
-        System.out.println("21- " + offsetDateTime2);
-
+    private synchronized static void inc(int number) {
+        numbers.add(number);
     }
 
+    private synchronized static void show() {
+        System.out.println(numbers);
+    }
+
+    // Transformei em static para o main acessar
+    static Runnable inc = () -> {
+        for (int i = 0; i < 100_000; i++) {
+            inc(i);
+        }
+    };
+
+    static Runnable dec = () -> {
+        for (int i = 100_000; i > 0; i--) {
+            inc(i);
+        }
+    };
+
+    static Runnable show = () -> {
+        for (int i = 0; i < 250_000; i++) {
+            show();
+        }
+    };
+
+    public static void main(String[] args) {
+        new Thread(inc).start();
+        new Thread(dec).start();
+        new Thread(show).start();
+    }
 }
