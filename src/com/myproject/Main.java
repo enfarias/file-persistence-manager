bba560f6feda1a7d32fee690ea588987b6333099
Edson Ney de Farias <edson.ney@gmail.com>
2026-05-05 11:52:20 -0300

    Thread Class and Runnable Interface Part 2

--- src/com/myproject/Main.java
+++ src/com/myproject/Main.java
@@ -1,42 +1,57 @@
 package com.myproject;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Duration;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Main {
 
-    private static List<Integer> numbers = new ArrayList<>();
+    private final static Queue<Integer> numbers = new LinkedBlockingQueue<>(250_000);
 
-    private synchronized static void inc(int number) {
+    private static void inc(int number) {
         numbers.add(number);
     }
 
-    private synchronized static void show() {
+    private static void show() {
         System.out.println(numbers);
     }
 
     // Transformei em static para o main acessar
     static Runnable inc = () -> {
-        for (int i = 0; i < 100_000; i++) {
+        for (int i = 0; i < 100; i++) {
             inc(i);
         }
     };
 
     static Runnable dec = () -> {
-        for (int i = 100_000; i > 0; i--) {
+        for (int i = 0; i > -100; i--) {
             inc(i);
         }
     };
 
     static Runnable show = () -> {
-        for (int i = 0; i < 250_000; i++) {
+        for (int i = 0; i < 100; i++) {
             show();
         }
     };
 
-    public static void main(String[] args) {
-        new Thread(inc).start();
-        new Thread(dec).start();
-        new Thread(show).start();
+    public static void main(String[] args) throws InterruptedException {
+
+        var execInc = new Thread(inc);
+        execInc.start();
+        execInc.setName("execInc");
+        execInc.join(Duration.ofSeconds(8));
+
+        var execDec = new Thread(dec);
+        execDec.start();
+        execDec.setName("execDec");
+
+        var execShow = new Thread(show);
+        execShow.start();
+        execShow.setName("execShow");
+
+        System.out.println(execInc.getName());
+        System.out.println(execDec.getName());
+        System.out.println(execShow.getName());
     }
 }
